5ace8c77abee55931d8a6c66b57d8ccbc42d31b8
Federico <federico.creti87@google.com>
2018-05-30 22:12:19 +0200

    Exercise T09b.09 done

--- Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.09-Exercise-AddTheViewModel/app/src/main/java/com/example/android/todolist/MainViewModel.java
@@ -1,11 +1,35 @@
 package com.example.android.todolist;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Database;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
+
+import java.util.List;
+
 // TODO (1) make this class extend AndroidViewModel and implement its default constructor
-public class MainViewModel {
+public class MainViewModel extends AndroidViewModel {
+    private final String TAG = MainViewModel.class.getSimpleName();
 
     // TODO (2) Add a tasks member variable for a list of TaskEntry objects wrapped in a LiveData
+    private LiveData<List<TaskEntry>> tasksEntry;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        Log.d(TAG, "Actively retrieving the tasks from DataBase");
+        AppDatabase database = AppDatabase.getInstance(this.getApplication());
+        tasksEntry = database.taskDao().loadAllTasks();
+    }
 
     // TODO (4) In the constructor use the loadAllTasks of the taskDao to initialize the tasks variable
 
     // TODO (3) Create a getter for the tasks variable
+    public LiveData<List<TaskEntry>> getTasksEntry() {
+        return tasksEntry;
+    }
 }
